9160f76df4e8a539be269fefd40b54aa1e6ad87f
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-10 22:16:44 +0530

    ADDED- Ride Class, Ride Method And Test Case For Multiple Rides

--- src/test/java/com/bridgelabz/test/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/test/InvoiceGeneratorTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.test;
 
 import com.bridgelabz.service.InvoiceGenerator;
+import com.bridgelabz.service.Ride;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,8 +11,8 @@ public class InvoiceGeneratorTest {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double totalFare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(25, totalFare,0.0);
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare,0.0);
     }
 
     @Test
@@ -19,7 +20,17 @@ public class InvoiceGeneratorTest {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
-        double totalFare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(5, totalFare,0.0);
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare,0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                        new Ride(0.1, 1)
+                        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
