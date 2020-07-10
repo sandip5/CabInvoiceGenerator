ec9d8f77622fd6a31cf4a5c5b92a853711b5d7f0
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-11 02:28:44 +0530

    REFACTORED- Calculate Fare Method

--- src/main/java/com/bridgelabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/service/InvoiceGenerator.java
@@ -9,7 +9,7 @@ public class InvoiceGenerator {
     public double calculateFare(double distance, int time, int COST_PER_TIME,
                                 double MINIMUM_COST_PER_KM, double MINIMUM_FARE) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-        if( totalFare < MINIMUM_FARE )
+        if (totalFare < MINIMUM_FARE)
             return MINIMUM_FARE;
         return totalFare;
     }
@@ -18,20 +18,20 @@ public class InvoiceGenerator {
         double totalFare = 0;
         String rideCategory = InvoiceSummary.rideCategory;
         for (Ride ride : rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time, rideCategory );
+            totalFare += this.calculateFare(ride.distance, ride.time, rideCategory);
         }
         return new InvoiceSummary(totalFare, rides.length, rideCategory);
     }
 
     public double calculateFare(double distance, int time, String rideCategory) {
-        switch(rideCategory){
+        switch (rideCategory) {
             case "PREMIUM_RIDES":
                 COST_PER_TIME = 2;
                 MINIMUM_COST_PER_KM = 15.0;
                 MINIMUM_FARE = 20.0;
                 return calculateFare(distance, time, COST_PER_TIME, MINIMUM_COST_PER_KM, MINIMUM_FARE);
             case "NORMAL_RIDES":
-                int COST_PER_TIME = 1;
+                COST_PER_TIME = 1;
                 MINIMUM_COST_PER_KM = 10.0;
                 MINIMUM_FARE = 5.0;
                 return calculateFare(distance, time, COST_PER_TIME, MINIMUM_COST_PER_KM, MINIMUM_FARE);
--- src/test/java/com/bridgelabz/test/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/test/InvoiceGeneratorTest.java
@@ -17,7 +17,7 @@ public class InvoiceGeneratorTest {
         int time = 5;
         String rideCategory = "NORMAL_RIDES";
         double fare = invoiceGenerator.calculateFare(distance, time, rideCategory);
-        Assert.assertEquals(25, fare,0.0);
+        Assert.assertEquals(25, fare, 0.0);
     }
 
     @Test
@@ -27,15 +27,15 @@ public class InvoiceGeneratorTest {
         int time = 1;
         String rideCategory = "NORMAL_RIDES";
         double fare = invoiceGenerator.calculateFare(distance, time, rideCategory);
-        Assert.assertEquals(5, fare,0.0);
+        Assert.assertEquals(5, fare, 0.0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        Ride[] rides = { new Ride(2.0, 5),
-                         new Ride(0.1, 1)
-                        };
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
 
         String rideCategory = "NORMAL_RIDES";
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(30.0, 2, rideCategory);
@@ -46,18 +46,18 @@ public class InvoiceGeneratorTest {
     @Test
     public void givenUserId_ShouldReturnInvoice() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        Ride[] rides1 = { new Ride(2.0, 5),
+        Ride[] rides1 = {new Ride(2.0, 5),
                 new Ride(0.1, 1),
-                new Ride(2.0,5)
+                new Ride(2.0, 5)
         };
-        Ride[] rides2 = { new Ride(5.0, 5),
+        Ride[] rides2 = {new Ride(5.0, 5),
                 new Ride(0.1, 8)
         };
         Map<Integer, Ride[]> usersRides = new HashMap<Integer, Ride[]>();
         int userId1 = 101;
         usersRides.put(userId1, rides1);
         int userId2 = 102;
-        usersRides.put(userId2,rides2);
+        usersRides.put(userId2, rides2);
         RideRepository rideRepository = new RideRepository();
         rideRepository.setUserRides(usersRides);
         InvoiceService invoiceService = new InvoiceService();
@@ -75,6 +75,6 @@ public class InvoiceGeneratorTest {
         int time = 5;
         String rideCategory = "PREMIUM_RIDES";
         double fare = invoiceGenerator.calculateFare(distance, time, rideCategory);
-        Assert.assertEquals(40, fare,0.0);
+        Assert.assertEquals(40, fare, 0.0);
     }
 }
